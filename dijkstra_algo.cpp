8f7b9ff89a83cfb4044ea33a4ca9b1a58add65da
PhenyoDitebo <work.riven100@gmail.com>
2026-02-11 20:39:00 -0500

    added comments

--- dijkstra_algo.cpp
+++ dijkstra_algo.cpp
@@ -1,16 +1,31 @@
+// -------------------------------- DIJSKSTRA -------------------------------
 # include <bits/stdc++.h>
 using namespace std;
 
 #define INF 999
 
-// ------------------------ DIJSKSTRA ------------------------
+
+// ----------------------------- HELPER VARIABLES ---------------------------
+
+// in graph theory, we need a way to represent nodes and the cost to travel between them
+// for example, if cost[0][1] = 5, that means the edge from node 0 -> 1 weights 5.
+// dist[100] keeps track of the current best distance from the source to every other node.
+// visted[100] will be a bool array to ensure we don't process the same node twice once
+// the shortest path has been confirmed.
+// parent[100] will be used to reconstruct the actual path (from end node to start node)
 
 int V, src, cost[100][100];
 int dist[100];
 bool visited[100] = {0};
+int parent[100];
 
-void innit() {
+// ------------------------------- INITIALIZATION ------------------------------
+
+// before algo starts, we first must assume all nodes
+
+void init() {
     for (int i = 0; i < V; i++) {
+        parent[i] = i;
         dist[i] = INF;
     dist[src] = 0;
     }
@@ -35,12 +50,14 @@ void dijkstra() {
         visited[nearest] = true;
 
         for (int adj = 0; adj < V; adj++) {
-            if (cost[nearest][adj] != INF && dist[adj] < dist[nearest]+cost[nearest][adj]) {
+            if (cost[nearest][adj] != INF && dist[adj] > dist[nearest]+cost[nearest][adj]) {
                 dist[adj] = dist[nearest]+cost[nearest][adj];
+                parent[adj] = nearest;
             }
         }
     }
 }
+
 
 
 int main(void) {
@@ -52,4 +69,6 @@ int main(void) {
     }
 
     cin >> src; 
+    init();
+    dijkstra();
 }
